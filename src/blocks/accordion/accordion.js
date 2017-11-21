2de5dd27349a590d30482587c27728fc5d60d6b8
Max <maxim00066@gmail.com>
2017-11-21 14:46:06 +0300

    :rocket: created accordion

--- src/blocks/accordion/accordion.js
+++ src/blocks/accordion/accordion.js
@@ -1,24 +1,38 @@
 $(document).ready(function() {
-    function close_accordion_section() {
-        $('.accordion .accordion-section-title').removeClass('active');
-        $('.accordion .accordion-section-content').slideUp(300).removeClass('open');
+  function close_accordion_section() {
+    $('.accordion .accordion-section-title').removeClass('active');
+    $('.accordion .accordion-section-content').slideUp(300).removeClass('open');
+  }
+
+  $('.accordion-section-title').click(function(e) {
+    // Grab current anchor value
+    var currentAttrValue = $(this).attr('href');
+
+    if($(e.target).is('.active')) {
+      close_accordion_section();
+    }else {
+      close_accordion_section();
+
+      // Add active class to section title
+      $(this).addClass('active');
+      // Open up the hidden content panel
+      $('.accordion ' + currentAttrValue).slideDown(300).addClass('open');
     }
 
-    $('.accordion-section-title').click(function(e) {
-        // Grab current anchor value
-        var currentAttrValue = $(this).attr('href');
+    e.preventDefault();
+  });
+});
 
-        if($(e.target).is('.active')) {
-            close_accordion_section();
-        }else {
-            close_accordion_section();
 
-            // Add active class to section title
-            $(this).addClass('active');
-            // Open up the hidden content panel
-            $('.accordion ' + currentAttrValue).slideDown(300).addClass('open');
-        }
+$('.accordion__nav-link').bind('click.smoothscroll',function (e) {
+  e.preventDefault();
 
-        e.preventDefault();
-    });
+  var target = this.hash,
+  $target = $(target);
+
+  $('html, body').stop().animate({
+    'scrollTop': $target.offset().top
+  }, 700, 'swing', function () {
+    window.location.hash = target;
+  });
 });
